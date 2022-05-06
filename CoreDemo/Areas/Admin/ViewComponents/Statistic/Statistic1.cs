cf947f3af882c17b0560ac66c4c51737e4757241
Selvi Yildirim <selviyldrm96@gmail.com>
2022-05-06 16:47:32 +0300

    106.ders sonu

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count(); //toplam blog sayısı
+            ViewBag.v2 = c.Contacts.Count(); //mesaj sayısı
+            ViewBag.v3 = c.Comments.Count(); //yorum sayısı
+
+            string api = "b4dcb2e1763d69f20dd11b0657069d44";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value; //sıcaklığı çekmek için 
+            return View();
+        }
+        }
+    }
